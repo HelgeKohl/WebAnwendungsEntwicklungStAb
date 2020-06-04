8fb94907f33d853906a586dbdd618cb757e1fb40
Helge Kohl <h.kohl@oth-aw.de>
2020-06-04 19:44:31 +0200

    static file handling

--- Kohl_Helge/src/server/server.js
+++ Kohl_Helge/src/server/server.js
@@ -1,22 +1,15 @@
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
-var qs = require('querystring');
-var url = require('url');
+
 
 http.createServer(function (request, response) {
-    let reqUrl = url.parse(request.url, true);
-    
     var filePath = '.' + request.url;
     if (filePath == './') {
         filePath = './static/index.html';
     }
     console.log(filePath)
-    if(filePath == './static/'){
-
-    }
-    // ggf. Prüfung einbauen das nicht alle Files ausgeliefert werden
-    else {
+    if(/^\.\/static\//.test(filePath)){
         var extname = String(path.extname(filePath)).toLowerCase();
 
         var mimeTypes = {
@@ -25,16 +18,6 @@ http.createServer(function (request, response) {
             '.css': 'text/css',
             '.json': 'application/json',
             '.png': 'image/png',
-            '.jpg': 'image/jpg',
-            '.gif': 'image/gif',
-            '.svg': 'image/svg+xml',
-            '.wav': 'audio/wav',
-            '.mp4': 'video/mp4',
-            '.woff': 'application/font-woff',
-            '.ttf': 'application/font-ttf',
-            '.eot': 'application/vnd.ms-fontobject',
-            '.otf': 'application/font-otf',
-            '.wasm': 'application/wasm'
         };
 
         var contentType = mimeTypes[extname] || 'application/octet-stream';
@@ -56,6 +39,12 @@ http.createServer(function (request, response) {
                 response.writeHead(200, { 'Content-Type': contentType });
                 response.end(content, 'utf-8');
             }
+        });
+    }
+    else{
+        fs.readFile('./404.html', function(error, content) {
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.end(content, 'utf-8');
         });
     }
 }).listen(8080);
