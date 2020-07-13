50e88731388ea629d86d54336613589ef9e7601c
Helge Kohl <h.kohl@oth-aw.de>
2020-07-13 18:38:41 +0200

    another backup

--- Kohl_Helge/src/server/static/js/main.js
+++ Kohl_Helge/src/server/static/js/main.js
@@ -2,7 +2,6 @@ var datetime = Vue.component('vue-datetime', window.VueDatetime.Datetime);
 datetime.options.props.minuteStep.default = 5
 datetime.options.props.inputClass.default = "form-control mr-sm-2"
 
-
 var filter = new Vue({
     el: '#app',
     data: {
